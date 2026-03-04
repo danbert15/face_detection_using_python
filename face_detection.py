50a9d5f2f9a610f582b0ba225fd8b4a4343ba2f4
Dan <danbertbacer@gmail.com>
2026-03-04 23:01:14 +0800

    Code Almost Done

--- face_detection.py
+++ face_detection.py
@@ -7,3 +7,13 @@ def main():
     if not cap.isOpened():
         print("Error: Could not open camera")
         return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame.")
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,miSize=(30, 30))
